56381784072a5f48ff6a2768a8044e795c3cf73c
MiaToftt <92541557+MiaToftx@users.noreply.github.com>
2023-07-25 17:25:50 +0200

    Testing

--- assets/JS/goToPageEvents.js
+++ assets/JS/goToPageEvents.js
@@ -2,19 +2,19 @@ let homeButton = document.getElementById("home")
 homeButton.addEventListener("click", goToHomePage)
 
 async function goToHomePage() {
-  document.location = "/docs/HTML/index.html"
+  document.location = "/assets/HTML/frontpage.html"
 }
 
 let contactInfoButton = document.getElementById("contact")
 contactInfoButton.addEventListener("click", goToContactPage)
 
 function goToContactPage() {
-  document.location.href = "/docs/HTML/contactpage.html"
+  document.location.href = "/assets/HTML/contactpage.html"
 }
 
 let aboutMeButton = document.getElementById("about")
 aboutMeButton.addEventListener("click", goToAboutMePage)
 
 function goToAboutMePage() {
-  document.location.href = "/docs/HTML/aboutmepage.html"
+  document.location.href = "/assets/HTML/aboutmepage.html"
 }
